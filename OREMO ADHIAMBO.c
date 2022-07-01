4a1a064bb05a4c791dc66a659c92316d07af4388
eveoremo <106369047+eveoremo@users.noreply.github.com>
2022-07-01 11:44:51 +0300

    Add files via upload

---
+++ OREMO ADHIAMBO.c
@@ -0,0 +1,36 @@
+/*
+Simple Calculator
+created by Everline Oremo
+on July 1
+C89
+MIT license
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nm1,num2,sum,diff,product;
+    float quotient
+    printf('\tsimple caculator!\n');
+    printf("enter two numbers:!\n");
+    scanf(%d%d,$num1,$num2);
+
+    //computations
+    sum=num1+num2;
+    diff=num1-num2;
+    product=num1*num2;
+    quotient=(float)num1/num2
+
+    //output
+    printf("%d+%d=%d\n",num1,num2,sum);
+    printf("%d-%d=%d\n",num1,num2,diff);
+    printf("%d*%d=d\n",num1,num2,product);
+    printf("%d/%d=%f\n",num1,num2,quotient);
+
+
+
+
+    return 0;
+}
